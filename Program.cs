5a590066a24f8068c2ffd48740c110a06bfeea95
Dmitriy209 <80581870+Dmitriy209@users.noreply.github.com>
2024-06-25 11:05:30 +0500

    Ver.1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace BracketExpression
 {
@@ -10,97 +6,92 @@ namespace BracketExpression
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
+            const string ButtonExit = "exit";
 
-            Console.WriteLine("Введите скобки:");
-            string line = Console.ReadLine();
+            bool isRunning = true;
 
-            //char[] tempLine = new char[line.Length];
-
-            //for (int i = 0; i < line.Length; i++)
-            //{
-            //    tempLine[i] = line[i];
-            //}
-
-            char tempParenthesis;
             char openParenthesis = '(';
             char closeParenthesis = ')';
 
-            int currentDepth = 0;
-            int maxDepth = 0;
-            int expectedDepth = 0;
-
-            int numberOpenParenthesis = 0;
-            int numberCloseParenthesis = 0;
-
-            int numberOpenParenthesisRow = 0;
-            int numberCloseParenthesisRow = 0;
-
-            int lastIndex = line.Length - 1;
-
-            bool isParentheticalCorrect = false;
-
-            bool lastTurnOpenParenthesis = false;
-            bool lastTurnCloseParenthesis = false;
-
-            string[] lineOpenParenthesis = line.Split(closeParenthesis);
-            string[] lineCloseParenthesis = line.Split(openParenthesis);
-
-            if (line[0] == closeParenthesis || line[lastIndex] == openParenthesis || lineOpenParenthesis.Length != lineCloseParenthesis.Length)
+            while (isRunning)
             {
-                isParentheticalCorrect = false;
-            }
-            else if (lineOpenParenthesis.Length == lineCloseParenthesis.Length)
-            {
-                for (int i = 0; i < line.Length; i++)
+                Console.WriteLine("Введите строку из круглых скобок или exit:");
+                string lineInput = Console.ReadLine();
+
+                switch (lineInput)
                 {
-                    
+                    case ButtonExit:
+                        isRunning = false;
+                        break;
 
-                    if (line[i] == openParenthesis)
-                    {
-                        numberOpenParenthesis++;
-                        currentDepth = numberOpenParenthesis - numberCloseParenthesis;
-                    }
-                    else if (line[i] == closeParenthesis)
-                    {
-                        numberCloseParenthesis++;
-                        currentDepth --;
-                    }
+                    default:
+                        int currentDepth = 0;
+                        int maxDepth = 0;
 
-                    if (currentDepth > maxDepth)
-                    {
-                        maxDepth = currentDepth;
-                    }
-                    
+                        int numberOpenParenthesis = 0;
+                        int numberCloseParenthesis = 0;
+
+                        int lastIndex = lineInput.Length - 1;
+
+                        bool isParentheticalCorrect = false;
+
+                        string[] lineOpenParenthesis = lineInput.Split(closeParenthesis);
+                        string[] lineCloseParenthesis = lineInput.Split(openParenthesis);
+
+                        if (lineInput[0] == closeParenthesis || lineInput[lastIndex] == openParenthesis || lineOpenParenthesis.Length != lineCloseParenthesis.Length)
+                        {
+                            isParentheticalCorrect = false;
+                        }
+                        else if (lineOpenParenthesis.Length == lineCloseParenthesis.Length)
+                        {
+                            for (int i = 0; i < lineInput.Length; i++)
+                            {
+                                if (lineInput[i] == openParenthesis)
+                                {
+                                    numberOpenParenthesis++;
+                                    currentDepth = numberOpenParenthesis - numberCloseParenthesis;
+                                }
+                                else if (lineInput[i] == closeParenthesis)
+                                {
+                                    numberCloseParenthesis++;
+                                    currentDepth--;
+
+                                    if (numberCloseParenthesis > numberOpenParenthesis)
+                                    {
+                                        i = lineInput.Length;
+                                    }
+                                }
+
+                                if (currentDepth > maxDepth)
+                                {
+                                    maxDepth = currentDepth;
+                                }
+                            }
+
+                            if (currentDepth < 0)
+                            {
+                                isParentheticalCorrect = false;
+                            }
+                            else
+                            {
+                                isParentheticalCorrect = true;
+                            }
+                        }
+
+                        if (isParentheticalCorrect == true)
+                        {
+                            Console.WriteLine($"Ваша строка: {lineInput} и она корректна, " +
+                                $"максимальная глубина равняется {maxDepth}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Строка {lineInput} - некорректна.");
+                        }
+                        break;
                 }
-
-                //if (numberOpenParenthesis == numberCloseParenthesis)
-                //{
-                isParentheticalCorrect = true;
-                //}
-
             }
 
-            Console.WriteLine($"Текущая глубина {currentDepth}\n" +
-                $"Максимальная глубина {maxDepth}");
-
-            if (isParentheticalCorrect == true)
-            {
-                Console.WriteLine($"Ваша строка: {line} и она корректна.");
-            }
-            else if (isParentheticalCorrect == false)
-            {
-                Console.WriteLine($"Строка {line} - некорректна.");
-            }
-            else
-            {
-                Console.WriteLine("Необработанное исключение.");
-            }
-            Console.WriteLine("Конец программы");
-
-
-            }
+            Console.WriteLine("Программа завершена.");
         }
     }
 }
